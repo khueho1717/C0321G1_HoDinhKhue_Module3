46db40df1d9fb1dbb42e685591992d8f1b54d31a
hyuganejidn@gmail.com <xuan hung>
2021-06-10 09:11:08 +0700

    bai1

---
+++ bai_1_co_so_du_lieu_quan_he/thuc_hanh/creat_table.sql
@@ -0,0 +1,9 @@
+create database student_management;
+
+create  table student_management.student (
+id int not null,
+name varchar(45) null,
+age int null,
+contry varchar(45) null,
+primary key(id)
+ );

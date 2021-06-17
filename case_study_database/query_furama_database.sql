71fd2e61997389d9259c70e918b00a0ff906dc65
hyuganejidn@gmail.com <xuan hung>
2021-06-17 08:16:18 +0700

    case

--- case_study_database/query_furama_database.sql
+++ case_study_database/query_furama_database.sql
@@ -25,13 +25,81 @@ order by lan_dat;
 
 -- task 5
 
+select khach_hang.id_khach_hang, khach_hang.ho_ten,loai_khach.ten_loai_khach,hop_dong.id_hop_dong,dich_vu.ten_dich_vu,hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc, sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) as tong_tien
+from khach_hang
+left join loai_khach on khach_hang.id_loai_khach=loai_khach.id_loai_khach
+left join hop_dong on khach_hang.id_khach_hang=hop_dong.id_khach_hang
+left join dich_vu on hop_dong.id_dich_vu=dich_vu.id_dich_vu
+left join hop_dong_chi_tiet on hop_dong.id_hop_dong=hop_dong_chi_tiet.id_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.id_dich_vu_di_kem=dich_vu_di_kem.id_dich_vu_di_kem
+group by khach_hang.id_khach_hang, hop_dong.id_hop_dong, tong_tien;
 
+
+-- task 6
+
+
+select dich_vu.id_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on dich_vu.id_loai_dich_vu=loai_dich_vu.id_loai_dich_vu
+join hop_dong on dich_vu.id_dich_vu=hop_dong.id_dich_vu
+where dich_vu.id_dich_vu not in (select hop_dong.id_dich_vu from hop_dong where (year(ngay_lam_hop_dong)=2019) and (month(ngay_lam_hop_dong) in (01,02,03)));
+
+-- task 7
+
+select dich_vu.id_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.so_nguoi_toi_da,dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on dich_vu.id_loai_dich_vu=loai_dich_vu.id_loai_dich_vu
+join hop_dong on dich_vu.id_dich_vu=hop_dong.id_dich_vu
+where year(ngay_lam_hop_dong)=2018 and year(ngay_lam_hop_dong) not in (select year(ngay_lam_hop_dong)	from hop_dong where (year(ngay_lam_hop_dong)=2019));
 
 
 
 -- task 8
+-- Cách 1:
+select khach_hang.ho_ten
+from khach_hang
+group by khach_hang.ho_ten;
+-- Cách 2:
 select distinct khach_hang.ho_ten
+from khach_hang
+group by khach_hang.ho_ten;
+-- Cách 3:
+select khach_hang.ho_ten
+from khach_hang
+union
+select khach_hang.ho_ten
 from khach_hang;
+
+-- task 9
+
+select month(ngay_lam_hop_dong) as "Thang", sum(dich_vu.chi_phi_thue +hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) as "Tong tien",count(hop_dong.id_khach_hang) as "So khach thue dich vu"
+from hop_dong
+join dich_vu on hop_dong.id_dich_vu=dich_vu.id_dich_vu
+join hop_dong_chi_tiet on hop_dong.id_hop_dong=hop_dong_chi_tiet.id_hop_dong
+join dich_vu_di_kem on hop_dong_chi_tiet.id_dich_vu_di_kem=dich_vu_di_kem.id_dich_vu_di_kem
+where year(ngay_lam_hop_dong)=2019
+group by month(ngay_lam_hop_dong);
+
+-- task 10
+
+select hop_dong.id_hop_dong,ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc,
+count(ID_hop_dong_chi_tiet) as so_luong_dich_vu_di_kem
+from hop_dong
+left join hop_dong_chi_tiet on hop_dong.id_hop_dong = hop_dong_chi_tiet.id_hop_dong
+group by id_hop_dong;
+
+-- task 11
+
+select khach_hang.id_khach_hang,ten_loai_khach, dich_vu_di_kem.ten_dich_vu_di_kem,
+dich_vu_di_kem.gia,dich_vu_di_kem.don_vi, hop_dong_chi_tiet.so_luong
+from khach_hang
+join loai_khach on khach_hang.id_loai_khach = loai_khach.id_loai_khach
+join hop_dong on hop_dong.id_khach_hang = khach_hang.id_khach_hang
+join hop_dong_chi_tiet on hop_dong.id_hop_dong = hop_dong_chi_tiet.id_hop_dong
+join dich_vu_di_kem on hop_dong_chi_tiet.id_dich_vu_di_kem = dich_vu_di_kem.id_dich_vu_di_kem
+where ten_loai_khach = 'Diamond' and ( dia_chi = 'vinh' or dia_chi = 'quang ngai');
+
+-- task 12
 
 
 
